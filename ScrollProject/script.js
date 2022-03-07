13c24005a3f329fbfc633b5fe51b8316cbf1a833
ismailcet <ismailcet3@gmail.com>
2022-03-08 00:21:29 +0300

    Scroll Project

--- ScrollProject/script.js
+++ ScrollProject/script.js
@@ -5,12 +5,13 @@ date.innerHTML=new Date().getFullYear();
 // Close List
 const toggle=document.querySelector('.nav-toggle');
 const listOpen=document.querySelector('.nav-links');
+const openNav=document.querySelector('.nav-center');
 toggle.addEventListener('click',showLinks);
 
 //Functions
 function showLinks(){
     listOpen.style.height=='0px'?listOpen.style.height='200px':listOpen.style.height='0px';
-    const openNav=document.querySelector('.nav-center');
+    
     openNav.addEventListener('click',(e)=>{
         if(e.target.classList.contains('scroll-link')){
             listOpen.style.height='0px'
@@ -30,3 +31,33 @@ function changeNav(){
         nav.classList.remove('fixed-nav');
     }
 }
+
+//Smooth Scroll
+const scrollLinks=document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link)=>{
+    link.addEventListener('click',(e)=>{
+        e.preventDefault();
+
+        const id=e.currentTarget.getAttribute('href').slice(1);
+        const element=document.getElementById(id);
+
+        const navHeight=nav.getBoundingClientRect().height;
+        const containerHeight=openNav.getBoundingClientRect().height;
+        const fixedNav=nav.classList.contains('fixed-nav');
+        let position=(element.offsetTop - navHeight)-50;
+        
+        if (!fixedNav) {
+            position = position - navHeight;
+          }
+          if (navHeight > 80) {
+            position = position + containerHeight;
+          }
+      
+          window.scrollTo({
+            left: 0,
+            top: position,
+          });
+          // close
+          listOpen.style.height = 0;
+    })
+})
